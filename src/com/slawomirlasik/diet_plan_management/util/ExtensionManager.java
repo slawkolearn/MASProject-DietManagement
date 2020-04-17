c392dce805d9c463da06c477b40c086049444d2b
Sławomir Lasik <slawomir.lasik@gmail.com>
2020-04-17 17:40:25 +0200

    Created constructor in ExtensionManager class for adding new extensions in the main extension container

--- src/com/slawomirlasik/diet_plan_management/util/ExtensionManager.java
+++ src/com/slawomirlasik/diet_plan_management/util/ExtensionManager.java
@@ -1,6 +1,39 @@
 package com.slawomirlasik.diet_plan_management.util;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 public class ExtensionManager {
 
     // TODO:SL implement managing extensions
+    // file name for storing extensions
+    private static final String EXTENSION_FILE = "extensionData.dtd";
+    // main container for applications extensions storage
+    private static Map<Class, List<ExtensionManager>> extensions = new HashMap<>();
+
+    public ExtensionManager() {
+        // this constructor adds the creating at the moment object to extensions conainter
+        // we use Class from this.getClass() method for map key
+        Class extensionKey = this.getClass();
+        // here we add the current object to a extension
+        List<ExtensionManager> extensionList = null;
+        // if there is already key with the class that we are adding (under this pointer) then we retrieve it
+        if(extensions.containsKey(extensionKey)){
+            extensionList = extensions.get(extensionKey);
+        }else{
+            // if not we are creating empty extension List for the new key
+            extensionList = new LinkedList<>(); // TODO:SL check which container should be more correct for this
+            // we add container of extensions to the main container with all extensions
+            extensions.put(extensionKey, extensionList);
+        }
+        // and we add this object that is being created to the retrieved / created list of extensions
+        extensionList.add(this);
+
+    }
+
+    public static void saveExtensionCurrentState(){
+
+    }
 }
