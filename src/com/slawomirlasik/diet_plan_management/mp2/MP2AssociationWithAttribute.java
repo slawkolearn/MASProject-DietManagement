eedbedaeb47a97d4f3a9c117a8ea9d718d687e26
Sławomir Lasik <slawomir.lasik@gmail.com>
2020-05-28 16:31:45 +0200

    Created stubs for developing association with attribute and composistion implementation in pure java

---
+++ src/com/slawomirlasik/diet_plan_management/mp2/MP2AssociationWithAttribute.java
@@ -0,0 +1,14 @@
+package com.slawomirlasik.diet_plan_management.mp2;
+
+public class MP2AssociationWithAttribute {
+
+    // TODO:SL implement association with attribute example (ManyToMany) using Recipe -- Ingredient with attribute RecipeIngredient
+
+    // again in our system we must implement this association in our system diagram as two associations
+    //
+
+
+    public static void main(String[] args) {
+
+    }
+}
