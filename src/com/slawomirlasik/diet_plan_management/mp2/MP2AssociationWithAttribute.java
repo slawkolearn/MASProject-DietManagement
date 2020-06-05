0d4d288f696c538b26ce4a4aec142a8e6a6e1809
Sławomir Lasik <slawomir.lasik@gmail.com>
2020-06-05 15:42:45 +0200

    Changed UMLDIetPlan Diagram : Now schedule is stored directly in each DietPlan

---
+++ src/com/slawomirlasik/diet_plan_management/mp2/MP2AssociationWithAttribute.java
@@ -0,0 +1,8 @@
+package com.slawomirlasik.diet_plan_management.mp2;
+
+public class MP2AssociationWithAttribute {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/slawomirlasik/diet_plan_management/mp2/MP2Composition.java
@@ -0,0 +1,4 @@
+package com.slawomirlasik.diet_plan_management.mp2;
+
+public class MP2Composition {
+}
